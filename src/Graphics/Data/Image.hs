a3e30638a22687e80d905db4556ace90ba1c72e3
Turab Jafri <trajafri@gmail.com>
2019-07-21 20:46:28 -0500

    Add Image docs, remove emptyScene, move rotate to Data.Image.

--- src/Graphics/Data/Image.hs
+++ src/Graphics/Data/Image.hs
@@ -1,12 +1,42 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Graphics.Data.Image
   ( Image(..)
-  , emptyScene
+  , rotate
   )
 where
 
-import           Graphics.Gloss
+import           Data.Angle
+import qualified Graphics.Gloss                as G
 
-data Image = Image {width :: Float , height :: Float , shapes :: [(Picture, Point)] } deriving Eq
+-- | A 2D Image.
+data Image = Image {width :: Float ,
+                    height :: Float ,
+                    shapes :: [(G.Picture, G.Point)] -- ^ collection of all Gloss Pictures that create the Image.
+                   } deriving Eq
 
-emptyScene :: Float -> Float -> Image
-emptyScene w h = Image { width = w, height = h, shapes = [] }
+-- | Rotates @i@ by @deg@ degrees in a counter-clockwise direction.
+--   Unlike 2htdp/image's rotate function, this function is not smart enough
+--   to reduce the rotated image's binding box to fit the actual image.
+--   Instead, it just creates a new binding box so that @i@'s binding box
+--   fits in it.
+rotate :: Float -> Image -> Image
+rotate deg Image {..} = Image newW newH
+  $ map (\(p, c) -> (G.rotate (negate deg) p, rotateC c)) shapes
+ where
+  newW =
+    width
+      * (abs . sine . Degrees $ 90 - deg)
+      + height
+      * (abs . sine . Degrees $ deg)
+  newH =
+    width
+      * (abs . sine . Degrees $ deg)
+      + height
+      * (abs . sine . Degrees $ 90 - deg)
+  rotateC :: (Float, Float) -> (Float, Float)
+  rotateC (x, y) =
+    ( x * (cosine . Degrees $ deg) + y * (negate . sine . Degrees $ deg)
+    , x * (sine . Degrees $ deg) + y * (cosine . Degrees $ deg)
+    )
+
